da044f5387f6a4a34df4ee39fe7a9820eddded25
palmerye <palmerye@gmail.com>
2018-08-29 10:33:09 +0800

    Update rollup.config.js

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,1 +1,18 @@
-
+export default {
+    input: './src/index.js',
+    output: [{
+        file: './dist/fe-router.iife.js',
+        format: 'iife',
+        sourceMap: 'inline'
+    },
+    {
+        file: './dist/fe-router.cjs.js',
+        format: 'cjs',
+        sourceMap: 'inline'
+    },
+    {
+        file: './dist/fe-router.umd.js',
+        format: 'umd',
+        sourceMap: 'inline'
+    }]
+}
